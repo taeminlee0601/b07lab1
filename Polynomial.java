7c4377c1732e8e7a91b2383aa21c32908bca70f8
Taemin Lee <taeminlee0601@gmail.com>
2024-05-16 10:30:38 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,45 @@
+public class Polynomial {
+    double[] coefficients;
+
+    public Polynomial() {
+           this.coefficients = new double[1];
+    }
+
+    public Polynomial(double[] coefficients) {
+        this.coefficients = new double[coefficients.length];
+
+        for (int i = 0; i < coefficients.length; i++) {
+            this.coefficients[i] = coefficients[i];
+        }
+    }
+
+    public Polynomial add(Polynomial toAdd) {
+        double[] resultArray = new double[Math.max(toAdd.coefficients.length, this.coefficients.length)];
+
+        for (int i = 0; i < resultArray.length; i++) {
+            if (i < toAdd.coefficients.length) {
+                resultArray[i] += toAdd.coefficients[i];
+            }
+
+            if (i < this.coefficients.length) {
+                resultArray[i] += this.coefficients[i];
+            }
+        }
+
+        return new Polynomial(resultArray);
+    }
+
+    public double evaluate(double value) {
+        double result = this.coefficients[0];
+
+        for (int i = 1; i < this.coefficients.length; i++) {
+            result += this.coefficients[i] * Math.pow(value, i);
+        }
+
+        return result;
+    }
+
+    public boolean hasRoot(double value) {
+        return evaluate(value) == 0;
+    }
+}
