b04c52b786c960b39de7bc50b2d5a845c9d6af43
Taemin Lee <taeminlee0601@gmail.com>
2024-05-31 01:05:14 -0400

    Modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class Driver {
     public static void main(String[] args) throws FileNotFoundException, IOException{
-        // default_test();
-        // test2();
+        default_test();
+        test2();
         test_file_in();
         test_file_in_add();
         test_file_in_multiply();
@@ -47,8 +47,8 @@ public class Driver {
         Polynomial s = p1.add(p2);
         Polynomial s1 = p2.add(p1);
 
-        System.out.println(s.toString());
-        System.out.println(s1.toString());
+        System.out.println("s(0) = " + s.evaluate(0));
+        System.out.println("s1(0) = " + s1.evaluate(0));
     }
 
     public static void test_file_in() throws FileNotFoundException {
@@ -60,21 +60,31 @@ public class Driver {
         Polynomial p2 = new Polynomial(f2);
         Polynomial p3 = new Polynomial(f3);
 
-        System.out.println(p1.toString());
-        System.out.println(p2.toString());
-        System.out.println(p3.toString());
+        System.out.println("p1(0.3) = " + p1.evaluate(0));
+        System.out.println("p2(0.3) = " + p2.evaluate(0));
+        System.out.println("p3(0.3) = " + p3.evaluate(0));
     }
 
     public static void test_file_in_add() throws FileNotFoundException {
-        File f1 = new File("p1.txt");
-        File f2 = new File("p2.txt");
+        File f1 = new File("p4.txt");
+        File f2 = new File("p5.txt");
 
         Polynomial p1 = new Polynomial(f1);
         Polynomial p2 = new Polynomial(f2);
 
         Polynomial sum = p1.add(p2);
 
-        System.out.println(sum.toString());
+        if (sum.hasRoot(2)) {
+            System.out.println("2 is a root of sum");
+        } else {
+            System.out.println("2 is not a root of sum");
+        }
+
+        if (sum.hasRoot(3)) {
+            System.out.println("3 is a root of sum");
+        } else {
+            System.out.println("3 is not a root of sum");
+        }
     }
 
     public static void test_file_in_multiply() throws FileNotFoundException {
@@ -86,7 +96,8 @@ public class Driver {
 
         Polynomial product = p1.multiply(p2);
 
-        System.out.println(product.toString());
+        System.out.println("product(12) = " + product.evaluate(12));
+        System.out.println("product(-2) = " + product.evaluate(-2));
     }
 
     public static void test_file_out() throws IOException {
